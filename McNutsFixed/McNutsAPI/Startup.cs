9903247eea51e18fa8bdc7c887fd9d9962d606ec
bernardoclaure <bernardoclaure13@gmail.com>
2022-04-02 10:11:53 -0400

    fixing branch error

--- McNutsFixed/McNutsAPI/Startup.cs
+++ McNutsFixed/McNutsAPI/Startup.cs
@@ -87,7 +87,8 @@ namespace McNutsAPI
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        { 
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
